fb939a462280f71c76816dfda43dbfe7530cbc69
srivastavashubhamji <38492475+srivastavashubhamji@users.noreply.github.com>
2019-08-27 20:52:23 -0700

    Adding Driver Method : main method
    main method added

--- src/HomeFrame/HomeFrame.java
+++ src/HomeFrame/HomeFrame.java
@@ -6563,6 +6563,30 @@ mysql> update tbl_books set status = 'I' where accid = 10003 and accno = 102;
         }
     }//  
 
+    public static void main(String args[]) {	// ----------------- Driver Method -----------------------	
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(HomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HomeFrame().setVisible(true);
+            }
+        });
+    }
 	
 	
 }// Class Ended...
